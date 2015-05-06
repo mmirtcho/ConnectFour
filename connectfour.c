a095966539e3848ca542f3733f6c46477abd42bd
Mark Mirtchouk <mmirtcho@laptop>
2015-05-05 20:13:27 -0400

    added instructions

--- connectfour.c
+++ connectfour.c
@@ -539,8 +539,11 @@ void ObtainInstructions(){
 	printf("Do you want to see instruction? (y/n)\n");
 	scanf("%s", buffer);
 	if(buffer[0]=='y'){
-		printf("STUFF\n");
-	}
+                printf("Welcome to our Connect Four Program\n");
+                printf("Your name (9 letters) will be used to compare to our existing high scores.\n");
+                printf("The difficulty can range from 1-10 and the moves can range from 1-7.\n");
+                printf("Good Luck; Have Fun!\n");
+        }
 }
 
 void SortInstructions(){
