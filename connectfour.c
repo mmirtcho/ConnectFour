088daf20e111289a8b3e24449c0282fa7b6fb589
Mark Mirtchouk <mmirtcho@laptop>
2015-05-05 22:31:26 -0400

    Added new input and output files and added Y to accept

--- connectfour.c
+++ connectfour.c
@@ -538,7 +538,7 @@ void ObtainInstructions(){
  	printf("\n");
 	printf("Do you want to see instruction? (y/n)\n");
 	scanf("%s", buffer);
-	if(buffer[0]=='y'){
+	if(buffer[0]=='y' || buffer[0]=='Y'){
                 printf("Welcome to our Connect Four Program\n");
                 printf("Your name (9 letters) will be used to compare to our existing high scores.\n");
                 printf("The difficulty can range from 1-10 and the moves can range from 1-7.\n");
